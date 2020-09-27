d05ea82452116d0045eccf922cd1d26b34c23561
Mustafa Yücesan <my96@live.nl>
2020-09-27 17:12:27 +0200

    Finished basic scrape, have a dict with multiple dicts, one dict per column.

---
+++ HelperMethods.py
@@ -0,0 +1,30 @@
+
+import re
+
+def sanitise(data):
+    return re.sub(r"\['|'\]|\[\"|\"\]|\\r|\\n", "", str(data))
+
+def reviewToDict(*args):
+    completeDict = {}
+    values = []
+    for arg in args:
+        isString = isinstance(arg, str)
+        if isString == True:
+            key = arg
+            continue
+        for item in arg:
+            if(isString == False):
+                item = sanitise(item.contents)
+                if item == "[]":
+                    item = "(((EmptyField)))"
+                values.append(item)
+        if len(values) != 0:
+            dictize = {key : values}
+            completeDict.update(dictize)
+    return completeDict
+
+def makeDict(*args):
+    adict = {}
+    for arg in args:
+        adict.update(arg)
+    return adict
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+import HelperMethods
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+from collections import defaultdict
 
-# Part 1: Data Discovery | Scrape 100 reviews per hotel booking site. Do 3 hotel booking site's. Booking, Trivago, 
-bookingPage = requests.get("https://www.booking.com/reviewlist.html?aid=304142&label=gen173nr-1FCAEoggI46AdIM1gEaKkBiAEBmAExuAEHyAEP2AEB6AEB-AECiAIBqAIDuAKw0o_7BcACAdICJDRiNmJkZDBmLTNjZmYtNDlkZC05MzRhLTM5MWE2M2ZjYTY2MtgCBeACAQ&sid=e76d27d037651f4921a5a8a6c425330f&cc1=tr&dist=1&pagename=silence-istanbul&srpvid=65e5a1203c000028&type=total&offset=10&rows=10")
+
+# Part 1: Data Discovery | Scrape 100 reviews per hotel booking site. Do 3 hotel booking site's. Hotels , ..., ...
+# TODO: For sentiment analysis we kight have trouble with multiple languages, check later.
+
+page = 1
+hotelsComReview = requests.get('https://nl.hotels.com/ho434942-tr-p' + str(page))
 # Hotel_Address,Additional_Number_of_Scoring,Review_Date,Average_Score,Hotel_Name,Reviewer_Nationality,Negative_Review,Review_Total_Negative_Word_Counts,Total_Number_of_Reviews,Positive_Review,Review_Total_Positive_Word_Counts,Total_Number_of_Reviews_Reviewer_Has_Given,Reviewer_Score,Tags,days_since_review,lat,lng
-# load kaggle dataset
-kaggle = pd.read_csv("reviews.csv") 
-print(bookingPage)
-if bookingPage.status_code == 200:
-    print("The page has been requested succesfully.")
-    soup = BeautifulSoup(bookingPage.content, "html.parser")
-    print(soup.prettify())
-    review = soup.find_all(class_="bui-avatar-block__title")
-    print(review)
+# load kaggle dataset12
+# kaggle = pd.read_csv("reviews.csv") # comment for faster processing
+# print(type(kaggle))
 
+if hotelsComReview.status_code == 200:
+    print("The page has been requested succesfully.")
+    soup = BeautifulSoup(hotelsComReview.content, "html.parser")
+    reviewers = soup.find_all(attrs={"class": "reviewer"})
+    review_desc = soup.find_all(attrs={"class": "description"})
+    review_score = soup.find_all(attrs={"class": "rating-score"})
+
+    thedictionary = HelperMethods.makeDict(HelperMethods.reviewToDict("name", reviewers), HelperMethods.reviewToDict("name", reviewers), HelperMethods.reviewToDict("description", review_desc), HelperMethods.reviewToDict("score", review_score))
     
+    # thedictionary = pd.DataFrame.from_dict(thedictionary)
+    print(thedictionary)
+
+
