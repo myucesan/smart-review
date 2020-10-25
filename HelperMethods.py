82959362b4a11afa863191684bffb4432f5192c8
Mustafa Yücesan <my96@live.nl>
2020-10-25 15:30:13 +0100

    Fixed end result, have all data labeled, except self written.

--- HelperMethods.py
+++ HelperMethods.py
@@ -1,5 +1,10 @@
 
 import re
+from textblob import TextBlob
+import pandas as pd
+from langdetect import detect_langs
+from sqlalchemy import create_engine # pip install mysqlclient in order for sqlalchemy to use mysql drivers
+
 
 def sanitise(data):
     return re.sub(r"\['|'\]|\[\"|\"\]|\\r|\\n", "", str(data))
@@ -28,3 +33,38 @@ def makeDict(*args):
     for arg in args:
         adict.update(arg)
     return adict
+
+# FIXME: Not always correct, double check.
+# FIXME array wordt er niet goed ingezet
+def detectLanguage(df):
+    languages = []
+    for text in df[df["description"] != "null"]["description"].values:
+        languages.append(str(detect_langs(text)[0])[0:2])
+    df["language"] = pd.DataFrame.from_dict(languages)
+
+#FIXME array wordt er niet goed ingezet
+def detectPolarity(df):
+    sentiment = []
+    for text in df[df["description"] != "null"]["description"].values:
+        text = TextBlob(text).sentiment
+        text = 1 if text[0] > 0 else 0
+        sentiment.append(str(text))
+    df["sentiment"] = pd.DataFrame.from_dict(sentiment)
+
+#FIXME array wordt er niet goed ingezet
+def detectScore(df):
+    score_rate = []
+    for score in df["score"].values:
+        rate = 1 if ((int(score.replace(",", "")) / 10) >= 7) else 0
+        score_rate.append(rate)
+    print(score_rate)
+    # df["score_rate"] = pd.DataFrame.from_dict(score_rate)
+    lala = df["score_rate"] = pd.DataFrame.from_dict(score_rate)
+    return lala
+
+def storeTable(df, tableName):
+    # SQL Connection
+    engine = create_engine("mysql://deprak1q_deds:uibUzlt0]sqZ@depraktischewinkel.nl/deprak1q_deds_ass1")
+    connection = engine.connect()
+
+    df.to_sql(tableName, engine)
